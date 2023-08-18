98727fd41690dadea6bc3984150bbd244b868367
sheshubittu <shashankpetkar007@gmail.com>
2023-08-18 16:35:54 +0530

    allowed host * added

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'sth^hh8e+g%jo5(zr7+ozv0)+uq&t93u!i@2mp*kfu2+xog2b0'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','https://domaindjangoapp2.azurewebsites.net/']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
